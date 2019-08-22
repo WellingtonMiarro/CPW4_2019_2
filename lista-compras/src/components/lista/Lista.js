f88c25af615723df994d1e618a44098f6a7831b0
Wellington Miarro Ferreira <42249348+WellingtonMiarro@users.noreply.github.com>
2019-08-22 20:57:00 -0300

    Carregando as listas na Pagina Inicial

--- lista-compras/src/components/lista/Lista.js
+++ lista-compras/src/components/lista/Lista.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import Menu from '../menu/Menu';
 
+import paginaAnterior from '../../img/paginaAnterior.png';
+
 class Lista extends Component {
   render() {
     return (
       <div>
-       <Menu titulo="Minha Lista"/>
+       <Menu 
+       logo ={paginaAnterior}
+       paginaAnterior="/"
+       titulo="Minha Lista"/>
         Lista
       </div>
     );
--- lista-compras/src/components/lista/Listas.js
+++ lista-compras/src/components/lista/Listas.js
@@ -1,11 +1,52 @@
 import React, { Component } from 'react';
+
 import  Menu from '../menu/Menu';
+import logo from '../../img/logo.png';
+
+import ListaService from '../../services/ListaService';
 class Listas extends Component {
+  constructor(){
+    /** 
+    Toda vez que criar um construtor em um componente react, 
+    lembre-se de invocar o construtor da classe mae Component na primeira linha de codigo
+    */
+  super();
+
+    this.state = {
+      listas: []
+    }
+    this.service = new ListaService();
+  }
+
+  componentDidMount(){
+
+    //const listasRecuperadas 
+    const listas = this.service.recuperarListas();
+    this.setState({listas}); //this.setState({listas: listasRecuperadas });
+   
+  }
+
+
   render() {
+    const listas = this.state.listas.map(lista =>(
+      <li key={lista._id}>{lista.nome}</li>
+    ));
     return (
       <div>
-        <Menu titulo="Lista de Compras"/>
-        Listas
+        <Menu 
+        logo={logo}
+        paginaAnterior="/"
+        titulo="Lista de Compras"/>
+
+        <div className="conteiner">
+            <div>
+              <h2>Minhas Listas</h2>
+              <ul>
+                  {listas}
+              </ul>
+            </div>
+        </div>   
+        
       </div>
     );
   }
